fb36d351e9d11083c4fdd2649bee53b12bf79e23
aomkar2421 <omkarjagtap1651@gmail.com>
2024-04-15 13:59:15 +0530

    15 april

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const routes = require('./routes/main')
 const hbs = require('hbs')
 const mongoose = require('mongoose')
 const Detail = require('./models/Detail')
+const Slider = require('./models/Slider')
 const app = express()
 
 //routes and static files
@@ -42,6 +43,30 @@ mongoose.connect("mongodb://127.0.0.1:27017/servicehub")
 //         }
 //     ] 
 // })
+
+// Slider.create([
+//     {
+//         title : 'Service 1',
+//         subtitle : 'Service 1 Description',
+//         imgUrl : 'static/images/slider1.jpg',
+//         class : 'active
+//     },
+//     {
+//         title : 'Service 2',
+//         subtitle : 'Service 2 Description',
+//         imgUrl : 'static/images/slider2.jpg'
+//     },
+//     {
+//         title : 'Service 3',
+//         subtitle : 'Service 3 Description',
+//         imgUrl : 'static/images/slider3.jpg'
+//     },
+//     {
+//         title : 'Service 4',
+//         subtitle : 'Service 4 Description',
+//         imgUrl : 'static/images/slider4.jpg'
+//     }
+// ])
 
 app.listen(4500, () => {
     console.log("Server Started")
--- src/routes/main.js
+++ src/routes/main.js
@@ -2,11 +2,14 @@ const express = require("express")
 const {route} = require('express/lib/application')
 const routes = express.Router()
 const Detail = require("../models/Detail")
+const Slider = require("../models/Slider")
 
 routes.get('', async (req,resp) => {
     let details =await Detail.findOne({ "_id":"661b7e629303ab08bdf2d12d" });
+    let slider =await Slider.find();
     resp.render('index',{
-        details: details
+        details: details,
+        slider : slider
     })
 })
 
